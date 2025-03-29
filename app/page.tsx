457dcd3ce14e0cbec221664f6401a5567cdfaf35
Pranay Dutta <rick15176@gmail.com>
2025-03-29 13:24:40 +0530

    Add blur motion

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,32 @@ import AboutMe from "./_components/AboutMe";
 import AvatarSection from "./_components/AvatarSection";
 import Projects from "./_components/Projects";
 import Technologies from "./_components/Technologies";
+import * as motion from "motion/react-client";
 
 export default function Home() {
   return (
-    <Flex gap="2" direction="column" className="md:pr-28">
-      <AvatarSection />
-      <Heading mt="6" size="4" weight="medium">
-        About
-      </Heading>
-      <AboutMe />
-      <div className="mt-6">
-        <Technologies />
-      </div>
-      <Heading mt="6" size="4" weight="medium">
-        Projects
-      </Heading>
+    <motion.div
+      initial={{ filter: "blur(10px)" }}
+      animate={{ filter: "blur(0px)" }}
+      transition={{ duration: 0.2 }}
+    >
+      <Flex gap="2" direction="column" className="md:pr-28">
+        <AvatarSection />
+        <Heading mt="6" size="4" weight="medium">
+          About
+        </Heading>
+        <AboutMe />
+        <div className="mt-6">
+          <Technologies />
+        </div>
+        <Heading mt="6" size="4" weight="medium">
+          Projects
+        </Heading>
 
-      <Flex direction="column" className="gap-8" mt="3">
-        <Projects />
+        <Flex direction="column" className="gap-8" mt="3">
+          <Projects />
+        </Flex>
       </Flex>
-    </Flex>
+    </motion.div>
   );
 }
