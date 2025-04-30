d60cf396de9a57192bb170ccd211c553beecbcb9
Pranay Dutta <rick15176@gmail.com>
2025-04-30 21:34:26 +0530

    add new project 'Rflix'

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -29,13 +29,21 @@ const Projects = () => {
       {/* Reference Links */}
       <Flex gap="5">
         {repo && (
-          <Link target="_blank" className="flex gap-1 items-center" href={repo.href}>
+          <Link
+            target="_blank"
+            className="flex gap-1 items-center"
+            href={repo.href}
+          >
             <repo.icon size="15" />
             <Text className="text-sm">View Repo</Text>
           </Link>
         )}
         {website && (
-          <Link target="_blank" className="flex gap-1 items-center" href={website.href}>
+          <Link
+            target="_blank"
+            className="flex gap-1 items-center"
+            href={website.href}
+          >
             <website.icon size="15" />
             <Text className="text-sm">Website</Text>
           </Link>
@@ -48,6 +56,14 @@ const Projects = () => {
 const projects: ProjectType[] = [
   {
     id: 0,
+    name: "Rflix",
+    description:
+      "Rflix is a streaming website. This website uses TMDB api to fetch movies & tv shows and stream them using third party services.",
+    repo: { icon: FaGithub, href: "https://github.com/pranay-dutta/rflix" },
+    website: { icon: CiGlobe, href: "https://rflix1.vercel.app/" },
+  },
+  {
+    id: 1,
     name: "Issue Tracker",
     description:
       "A next js powered project uses next-auth for authentication. Where you can create, read, update, delete issues and assign issues to specific user.",
@@ -58,7 +74,7 @@ const projects: ProjectType[] = [
     website: { icon: CiGlobe, href: "https://issue-tracker-8uhd.vercel.app/" },
   },
   {
-    id: 1,
+    id: 2,
     name: "Gamehive",
     description:
       "A full stack game query application uses Rawg api to fetch the games. Where you can view all games based on platforms and genres.",
