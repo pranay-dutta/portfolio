7f40e5e88b52d8b541ef18b9fa61e238126b7943
Pranay Dutta <rick15176@gmail.com>
2025-05-01 21:09:37 +0530

    fix: scrollbar jump

--- app/_components/ProjectSection.tsx
+++ app/_components/ProjectSection.tsx
@@ -14,7 +14,6 @@ const ProjectSection = () => {
         <motion.p
           className="cursor-pointer text-neutral-300 text-sm hover:text-blue-300 inline"
           onClick={() => toggleOpen()}
-          whileTap={{ y: 2 }}
         >
           See what I created{" "}
           {isOpen ? <GoArrowUpRight className="inline" /> : <GoArrowDownRight className="inline" />}
@@ -24,13 +23,13 @@ const ProjectSection = () => {
       <AnimatePresence initial={false}>
         {isOpen && (
           <motion.div
-            className="flex flex-col gap-8 mt-3"
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: "auto" }}
-            exit={{ opacity: 0, height: 0 }}
+            className="flex flex-col gap-8 mt-6"
+            initial={{ opacity: 0, height: 0, y: 30, filter: "blur(5px)" }}
+            animate={{ opacity: 1, height: "auto", y: 0, filter: "blur(0px)" }}
+            exit={{ opacity: 0, height: 0, scale: 0, filter: "blur(5px)" }}
             transition={{
-              height: { duration: 0.5 },
-              opacity: { duration: 0.5 },
+              height: { duration: 0.4 },
+              opacity: { duration: 0.4 },
             }}
           >
             <Projects />
