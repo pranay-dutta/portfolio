a89ca6b7de0734682dea2aa1c7c806118d9c5689
Pranay Dutta <rick15176@gmail.com>
2025-06-12 01:07:30 +0530

    refactor: improve technology section with scroll velocity

--- app/_components/Technologies.tsx
+++ app/_components/Technologies.tsx
@@ -1,4 +1,3 @@
-import { Flex } from "@radix-ui/themes";
 import { IconType } from "react-icons";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { GrReactjs } from "react-icons/gr";
@@ -14,27 +13,35 @@ import {
   SiTypescript,
 } from "react-icons/si";
 import IconPill from "./IconPill";
+import ScrollVelocity from "../_ui/text-animations/ScrollVelocity/ScrollVelocity";
+interface Technology {
+  icon: IconType;
+  name: string
+}
 
-const Technologies = () => {
-  return (
-    <Flex align="center" gap="3" wrap="wrap">
-      {technologoies.map(({ icon, name }) => (
-        <IconPill key={name} icon={icon} content={name} isLinked={false} />
-      ))}
-    </Flex>
-  );
-};
-const technologoies: { icon: IconType; name: string }[] = [
+const firstLine: Technology[] = [
   { icon: SiJavascript, name: "Javascript" },
   { icon: SiTypescript, name: "Typescript" },
   { icon: GrReactjs, name: "React.js" },
   { icon: SiPrisma, name: "Prisma" },
   { icon: SiExpress, name: "Express.js" },
+];
+const secondLine: Technology[] = [
   { icon: SiTailwindcss, name: "Tailwind CSS" },
   { icon: SiBootstrap, name: "Bootstrap" },
   { icon: SiChakraui, name: "Chakra UI" },
   { icon: SiMui, name: "Material UI" },
   { icon: RiNextjsFill, name: "Next.js" },
   { icon: BiLogoPostgresql, name: "Postgres" },
-];
+]
+
+const firstLineDiv = <div className="flex gap-2 pe-2">{firstLine.map(({ icon, name }) => <IconPill key={name} icon={icon} content={name} isLinked={false} />)}</div>
+const secondLineDiv = <div className="flex gap-2 pe-2">{secondLine.map(({ icon, name }) => <IconPill key={name} icon={icon} content={name} isLinked={false} />)}</div>
+
+const Technologies = () => {
+  return (
+    <ScrollVelocity childrens={[firstLineDiv, secondLineDiv]} />
+  );
+};
+
 export default Technologies;
