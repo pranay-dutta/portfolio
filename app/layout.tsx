d42c394b435543cf881196320346479285435e19
Pranay Dutta <rick15176@gmail.com>
2025-12-22 21:36:01 +0530

    fix: radix-ui/colors lib import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import { Theme } from "@radix-ui/themes";
+import "@radix-ui/colors/black-alpha.css";
+import "@radix-ui/colors/mauve.css";
+import "@radix-ui/colors/violet.css";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
@@ -19,7 +22,9 @@ export default function RootLayout({
       <body className={`${spaceGrotesk.variable} antialiased`}>
         <Theme appearance="dark">
           <main>
-            <div className="lg:max-w-6/11 lg:ml-[22%] xl:max-w-4/11 xl:ml-[23%] md:p-8 md:pt-10 p-4">{children}</div>
+            <div className="lg:max-w-6/11 lg:ml-[22%] xl:max-w-4/11 xl:ml-[23%] md:p-8 md:pt-10 p-4">
+              {children}
+            </div>
           </main>
         </Theme>
       </body>
